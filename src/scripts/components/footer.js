f78d738268abca988eee6af266b5512e92383024
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-01-03 21:44:58 -0700

    Updated footer, added github logo and link to project

--- src/scripts/components/footer.js
+++ src/scripts/components/footer.js
@@ -1,17 +1,23 @@
 import "styles/footer.css";
+import githubLogoPNG from "../../assets/imgs/github-mark.png";
 
 export default class Footer {
     constructor() {
         this.app = document.querySelector("#main-content-container");
 
         this.footer = document.createElement("footer");
-
         this.sourceContainer = document.createElement("div");
 
         this.sourceLink = document.createElement("a");
         this.sourceLink.target = "_blank";
-        this.sourceLink.textContent = "Michael F. 2024";
-        this.sourceLink.href = "https://github.com/michael-f-portfolio/";
+        this.sourceLink.textContent = "Copyright © 2024 Michael F.";
+        this.sourceLink.href = "https://github.com/michael-f-portfolio/Todo-List";
+
+        this.githubLogo = document.createElement("img");
+        this.githubLogo.src = githubLogoPNG;
+        this.sourceLink.appendChild(this.githubLogo);
+
+
         this.sourceContainer.appendChild(this.sourceLink);
         this.footer.appendChild(this.sourceContainer);
         this.app.appendChild(this.footer);
