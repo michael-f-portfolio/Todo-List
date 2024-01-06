8e1c7666433228e5564225e352c0ae32a1dd8237
Michael F <10968365+michael-f-portfolio@users.noreply.github.com>
2024-01-06 15:38:41 -0700

    todos now saved to local storage, removed placeholders

--- src/scripts/models/Todos.js
+++ src/scripts/models/Todos.js
@@ -2,29 +2,17 @@ import Todo from "./Todo";
 
 // Contains all the todos for the application and the CRUD methods for them
 export default class Todos {
-    constructor(todos = []) {
-        this.todos = todos;
-
-        //placeholder todos
-        this.todos.push(new Todo("My Title", "A short description.", "2024-01-05", "Low"));
-        this.todos.push(new Todo("My Very Very Very Very Descriptive Title",
-                                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
-                                 "sed do eiusmod tempor incididunt ut labore et dolore magna" +
-                                 " aliqua. Ut enim ad minim veniam, quis nostrud exercitation " +
-                                 "ullamco laboris nisi ut aliquip ex ea commodo consequat. exercitation ullamco la.",
-                                 "2024-05-29", "Normal"));
-        this.todos.push(new Todo("My Single Checklist Title", "A single thing to do.", "2024-01-05", "High",
-                                [{content: "Clean room"}]));
-        this.todos.push(new Todo("My Checklist Title", "A number of things to do.", "2024-01-05", "Urgent",
-                                [{content:"Wake up"},
-                                 {content: "Eat breakfast"},
-                                 {content: "Make bed"},
-                                 {content: "A rather long thing to do that is very important"},
-                                 {content: "Go to bed"}]));
+    constructor() {
+        this.todos = JSON.parse(localStorage.getItem("todos")) || [];
     }
 
     bindTodoListChange(callback) {
         this.onTodoListChanged = callback;
+    }
+
+    commitTodo(todos) {
+        this.onTodoListChanged(todos);
+        localStorage.setItem("todos", JSON.stringify(todos));
     }
 
     getTodo(todoId) {
@@ -37,7 +25,7 @@ export default class Todos {
                         inputValues.dueDateInput,
                         inputValues.priorityInput,
                         inputValues.checklistInput));
-        this.onTodoListChanged(this.todos);
+        this.commitTodo(this.todos)
     }
 
     editTodo(id, updatedInputValues) {
@@ -54,28 +42,19 @@ export default class Todos {
             } else {
                 return todo;
             }
-
-
-            // todo.id === id
-            //     ? new Todo(updatedInputValues.titleInput,
-            //         updatedInputValues.descriptionInput,
-            //         updatedInputValues.dueDateInput,
-            //         updatedInputValues.priorityInput,
-            //         updatedInputValues.checklistInput)
-            //     : todo;
         });
 
-        this.onTodoListChanged(this.todos);
+        this.commitTodo(this.todos);
     }
 
     deleteTodo(id) {
         this.todos = this.todos.filter((todo) => todo.id !== id);
-        this.onTodoListChanged(this.todos);
+        this.commitTodo(this.todos);
     }
 
     toggleCompleteTodo(id) {
         this.todos.find(todo => todo.id === id).toggleCompleted();
-        this.onTodoListChanged(this.todos);
+        this.commitTodo(this.todos);
     }
 
     toggleTodoChecklistItem(id, checklistItemId) {
@@ -85,8 +64,6 @@ export default class Todos {
                     .find(checklistItem =>
                     checklistItem.id === checklistItemId)
                     .toggleChecked();
-        this.onTodoListChanged(this.todos);
+        this.commitTodo(this.todos);
     }
-
-
 }
